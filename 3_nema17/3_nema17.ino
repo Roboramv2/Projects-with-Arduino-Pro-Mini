afc6eece1efe7a37140277a564a5b183346058cb
Roboramv2 <sriramkraja@gmail.com>
2021-06-04 14:31:38 +0530

    code for smooth nema17 drive

--- 3_nema17/3_nema17.ino
+++ 3_nema17/3_nema17.ino
@@ -1,39 +1,47 @@
 // Define pin connections & motor's steps per revolution
-const int dirPin = 2;
-const int stepPin = 3;
+const int dirPin1 = 2;
+const int stepPin1 = 3;
+const int dirPin2 = 4;
+const int stepPin2 = 5;
 const int stepsPerRevolution = 200;
 
 void setup()
 {
   // Declare pins as Outputs
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
+  pinMode(stepPin1, OUTPUT);
+  pinMode(dirPin1, OUTPUT);
+  pinMode(stepPin2, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
 }
 void loop()
 {
-  // Set motor direction clockwise
-  digitalWrite(dirPin, HIGH);
-
-  // Spin motor slowly
+  // Set motor direction
+  digitalWrite(dirPin1, HIGH);
+  digitalWrite(dirPin2, LOW);
+  // Spin motor
   for(int x = 0; x < stepsPerRevolution; x++)
   {
-    digitalWrite(stepPin, HIGH);
+    digitalWrite(stepPin1, HIGH);
+    digitalWrite(stepPin2, HIGH);
     delayMicroseconds(2000);
-    digitalWrite(stepPin, LOW);
+    digitalWrite(stepPin1, LOW);
+    digitalWrite(stepPin2, LOW);
     delayMicroseconds(2000);
   }
-  delay(1000); // Wait a second
+  delay(1000); 
   
-  // Set motor direction counterclockwise
-  digitalWrite(dirPin, LOW);
-
-  // Spin motor quickly
+  // Set motor direction
+  digitalWrite(dirPin1, LOW);
+  digitalWrite(dirPin2, HIGH);
+  // Spin motor
   for(int x = 0; x < stepsPerRevolution; x++)
   {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(1000);
+    digitalWrite(stepPin1, HIGH);
+    digitalWrite(stepPin2, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(stepPin1, LOW);
+    digitalWrite(stepPin2, LOW);
+    delayMicroseconds(2000);
   }
-  delay(1000); // Wait a second
+  delay(1000);
 }
